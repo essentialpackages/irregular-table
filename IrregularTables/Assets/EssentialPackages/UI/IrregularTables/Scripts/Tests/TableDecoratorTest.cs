41dcd414624ebc06f3a3236a69338c95f5efda57
Lars Wobus <lars.wobus@web.de>
2018-11-18 17:40:46 +0100

    3 of 5 unit tests for TableDecorator successful
    - TableDecorator must be reworked to pass the
    remainig 2 unit tests

--- IrregularTables/Assets/EssentialPackages/UI/IrregularTables/Scripts/Tests/TableDecoratorTest.cs
+++ IrregularTables/Assets/EssentialPackages/UI/IrregularTables/Scripts/Tests/TableDecoratorTest.cs
@@ -1,16 +1,26 @@
 ﻿using System.Collections;
 using System.Reflection;
+using System.Runtime.InteropServices;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
+using UnityEngine.UI;
 
 namespace EssentialPackages.UI.IrregularTables.Tests
 {
     public class TableDecoratorTest
     {
         private GameObject DummyGameObject { get; set; }
+        private GameObject ChildGameObject { get; set; }
         private TableDecorator TargetScript { get; set; }
         private TableStyle TableStyle { get; set; }
+        private Image ImageComponent { get; set; }
+
+        private void AddChildrenToDummyGameObject()
+        {
+            ChildGameObject = new GameObject();
+            ChildGameObject.GetComponent<Transform>().parent = DummyGameObject.transform;
+        }
         
         [OneTimeSetUp]
         public void OneTimeSetUp()
@@ -36,37 +46,62 @@ namespace EssentialPackages.UI.IrregularTables.Tests
         [TearDown]
         public void TearDown()
         {
+            ImageComponent = null;
+            Object.Destroy(ChildGameObject);
+            
             TargetScript = null;
             Object.Destroy(DummyGameObject);
         }
         
         [UnityTest]
-        public IEnumerator UpdateColors_Should_NotHaveAnyEffect_When_RootTransformWasNull() {
-            Assert.Fail();
+        public IEnumerator UpdateColors_Should_NotHaveAnyEffect_When_RootTransformWasNull()
+        {
+            TargetScript.UpdateColors(null, TableStyle);
+            
+            Assert.Pass();
             yield return null;
         }
         
         [UnityTest]
-        public IEnumerator UpdateColors_Should_NotHaveAnyEffect_When_TableStyleWasNull() {
-            Assert.Fail();
+        public IEnumerator UpdateColors_Should_NotHaveAnyEffect_When_TableStyleWasNull()
+        {
+            TargetScript.UpdateColors(DummyGameObject.transform, null);
+            
+            Assert.Pass();
             yield return null;
         }
 
         [UnityTest]
-        public IEnumerator UpdateColors_Should_NotHaveAnyEffect_When_NoChildreCouldBeFound() {
-            Assert.Fail();
+        public IEnumerator UpdateColors_Should_NotHaveAnyEffect_When_NoChildreCouldBeFound()
+        {
+            TargetScript.UpdateColors(DummyGameObject.transform, TableStyle);
+            
+            Assert.Pass();
             yield return null;
         }
         
         [UnityTest]
-        public IEnumerator UpdateColors_Should_NotHaveAnyEffect_When_NoImagesWereAttachedToChildren() {
-            Assert.Fail();
+        public IEnumerator UpdateColors_Should_NotHaveAnyEffect_When_NoImagesWereAttachedToChildren()
+        {
+            AddChildrenToDummyGameObject();
+            TargetScript.UpdateColors(DummyGameObject.transform, TableStyle);
+            
+            Assert.Pass();
             yield return null;
         }
         
         [UnityTest]
-        public IEnumerator UpdateColors_Should_ChangeImageColors_When_ImagesWereAttachedToChildren() {
-            Assert.Fail();
+        public IEnumerator UpdateColors_Should_ChangeImageColors_When_ImagesWereAttachedToChildren()
+        {
+            AddChildrenToDummyGameObject();
+            ImageComponent = ChildGameObject.AddComponent<Image>();
+            ImageComponent.color = Color.gray;
+            
+            TargetScript.UpdateColors(DummyGameObject.transform, TableStyle);
+
+            var expected = TableStyle.Colors[0];
+            Assert.AreEqual(ImageComponent.color, expected);
+            
             yield return null;
         }
     }
