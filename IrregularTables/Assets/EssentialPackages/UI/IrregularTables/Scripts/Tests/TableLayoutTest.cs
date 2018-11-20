d39a0e37f904088113d3f1b6423b70ba8fcfd18f
Lars Wobus <lars.wobus@web.de>
2018-11-20 23:33:50 +0100

    Unit tests for TableLayout finished

--- IrregularTables/Assets/EssentialPackages/UI/IrregularTables/Scripts/Tests/TableLayoutTest.cs
+++ IrregularTables/Assets/EssentialPackages/UI/IrregularTables/Scripts/Tests/TableLayoutTest.cs
@@ -5,7 +5,9 @@ using EssentialPackages.UI.IrregularTables.Data;
 using EssentialPackages.UI.IrregularTables.Interfaces;
 using EssentialPackages.UI.TextAdapters.Interfaces;
 using NUnit.Framework;
+using UnityEditor.VersionControl;
 using UnityEngine;
+using Debug = UnityEngine.Debug;
 using Object = UnityEngine.Object;
 
 namespace EssentialPackages.UI.IrregularTables.Tests
@@ -26,33 +28,55 @@ namespace EssentialPackages.UI.IrregularTables.Tests
 			
 			public GameObject CreateItem(TableCellType type, Transform parent)
 			{
-				return (Prefab == null) ? null : Object.Instantiate(Prefab);
+				return Object.Instantiate(Prefab, parent);
 			}
 		}
 
+		private class FakeScript : MonoBehaviour, ITextComponent
+		{
+			public string Id { get; set; }
+			public string Text { get; set; }
+		}
+
+		private FakeTable BrokenTable { get; set; }
+		private FakeTable IncompleteTable { get; set; } 
+		private FakeTable ValidTable { get; set; }
+		
 		private class FakeRegistry : ITextRegistry<ITextComponent>
 		{
 			public void Register(string id, ITextComponent text) {}
 		}
-
-		private TableLayout CreateInvalidLayoutUsingNullTable()
-		{
-			return new TableLayout(null, new FakeRegistry());
-		}
 		
 		private TableLayout CreateInvalidLayoutUsingBrokenFakeTable()
 		{
-			return new TableLayout(new FakeTable(null), new FakeRegistry());
+			return new TableLayout(BrokenTable, new FakeRegistry());
+		}
+		
+		private TableLayout CreateInvalidLayoutUsingIncompleteFakeTable()
+		{
+			return new TableLayout(IncompleteTable, new FakeRegistry());
 		}
 		
 		private TableLayout CreateValidLayoutUsingFakeTable()
 		{
-			return new TableLayout(new FakeTable(null), new FakeRegistry());
+			return new TableLayout(ValidTable, new FakeRegistry());
 		}
-        
+
+		[OneTimeSetUp]
+		public void OneTimeSetUp()
+		{
+			BrokenTable = new FakeTable(null);
+			IncompleteTable = new FakeTable(new GameObject());
+
+			var go = new GameObject();
+			go.AddComponent<FakeScript>();
+			ValidTable = new FakeTable(go);
+		}
+
 		[SetUp]
 		public void SetUp()
 		{
+			
 			var cell = new TableCell();
 			//Cells = new List<TableCell>(){};
 			//TargetScript = new TableLayout(new FakeTable(null), new FakeRegistry());
@@ -83,114 +107,357 @@ namespace EssentialPackages.UI.IrregularTables.Tests
 		}
 
 		[Test]
-		public void ExpandTable_Should_NotHaveAnyEffect_When_CollectionWasNull()
+		public void ExpandBrokenTable_Should_ThrowArgumentException_When_CreatingNewEmptyCell()
 		{
-			var tableLayout = CreateInvalidLayoutUsingNullTable();
-			
-			tableLayout.ExpandTable(null, null, 0, null);
-			
-			Assert.Pass();
+			Assert.Throws<ArgumentException>(() =>
+				{
+					var tableLayout = CreateInvalidLayoutUsingBrokenFakeTable();
+					var cells = new List<TableCell>()
+					{
+						{ new TableCell() { Type = TableCellType.Empty, Refs = new [] {""} } }
+					};
+					
+					tableLayout.ExpandTable(cells, null, 0, null);
+				}
+			);
 		}
 		
 		[Test]
-		public void ExpandTable_Should_NotHaveAnyEffect_When_EmptyCellHasNoReferences()
+		public void ExpandBrokenTable_Should_ThrowArgumentException_When_CreatingNewStaticText()
 		{
-			var tableLayout = CreateInvalidLayoutUsingNullTable();
+			Assert.Throws<ArgumentException>(() =>
+				{
+					var tableLayout = CreateInvalidLayoutUsingBrokenFakeTable();
+					var cells = new List<TableCell>()
+					{
+						{ new TableCell() { Type = TableCellType.StaticText, Refs = new [] {""} } }
+					};
+					
+					tableLayout.ExpandTable(cells, null, 0, null);
+				}
+			);
+		}
+		
+		[Test]
+		public void ExpandBrokenTable_Should_ThrowArgumentException_When_CreatingNewDynamicText()
+		{
+			Assert.Throws<ArgumentException>(() =>
+				{
+					var tableLayout = CreateInvalidLayoutUsingBrokenFakeTable();
+					var cells = new List<TableCell>()
+					{
+						{ new TableCell() { Type = TableCellType.DynamicText, Refs = new [] {""} } }
+					};
+					
+					tableLayout.ExpandTable(cells, null, 0, null);
+				}
+			);
+		}
+		
+		[Test]
+		public void ExpandBrokenTable_Should_ThrowArgumentException_When_CreatingNewRow()
+		{
+			Assert.Throws<ArgumentException>(() =>
+				{
+					var tableLayout = CreateInvalidLayoutUsingBrokenFakeTable();
+					var cells = new List<TableCell>()
+					{
+						{ new TableCell() { Type = TableCellType.Row } }
+					};
+					
+					tableLayout.ExpandTable(cells, null, 0, null);
+				}
+			);
+		}
+		
+		[Test]
+		public void ExpandBrokenTable_Should_ThrowArgumentException_When_CreatingNewColumn()
+		{
+			Assert.Throws<ArgumentException>(() =>
+				{
+					var tableLayout = CreateInvalidLayoutUsingBrokenFakeTable();
+					var cells = new List<TableCell>()
+					{
+						{ new TableCell() { Type = TableCellType.Column } }
+					};
+					
+					tableLayout.ExpandTable(cells, null, 0, null);
+				}
+			);
+		}
+		
+		[Test]
+		public void ExpandIncompleteTable_Should_CreateChildObject_When_CreatingNewEmptyCell()
+		{
+			var parent = new GameObject();
+			var tableLayout = CreateInvalidLayoutUsingIncompleteFakeTable();
 			var cells = new List<TableCell>()
 			{
-				{ new TableCell() { Type = TableCellType.Empty } }
+				{ new TableCell() { Type = TableCellType.Empty, Refs = new [] {""} } }
 			};
 			
-			tableLayout.ExpandTable(cells, null, 0, null);
+			tableLayout.ExpandTable(cells, parent.transform, 0, null);
 			
-			Assert.Pass();
+			Assert.AreEqual(1, parent.transform.childCount);
 		}
 		
 		[Test]
-		public void ExpandTable_Should_NotHaveAnyEffect_When_StaticTextCellHasNoReferences()
+		public void ExpandIncompleteTable_Should_ThrowNullReferenceException_When_CreatingNewStaticText()
 		{
-			var tableLayout = CreateInvalidLayoutUsingNullTable();
+			Assert.Throws<NullReferenceException>(() =>
+				{
+					var tableLayout = CreateInvalidLayoutUsingIncompleteFakeTable();
+					var cells = new List<TableCell>()
+					{
+						{ new TableCell() { Type = TableCellType.StaticText, Refs = new [] {""} } }
+					};
+					
+					tableLayout.ExpandTable(cells, null, 0, null);
+				}
+			);
+		}
+		
+		[Test]
+		public void ExpandIncompleteTable_Should_ThrowNullReferenceException_When_CreatingNewDynamicText()
+		{
+			Assert.Throws<NullReferenceException>(() =>
+				{
+					var tableLayout = CreateInvalidLayoutUsingIncompleteFakeTable();
+					var cells = new List<TableCell>()
+					{
+						{ new TableCell() { Type = TableCellType.DynamicText, Refs = new [] {""} } }
+					};
+					
+					tableLayout.ExpandTable(cells, null, 0, null);
+				}
+			);
+		}
+		
+		[Test]
+		public void ExpandIncompleteTable_Should_ThrowNullReferenceException_When_CreatingNewRow()
+		{
+			Assert.Throws<NullReferenceException>(() =>
+				{
+					var tableLayout = CreateInvalidLayoutUsingIncompleteFakeTable();
+					var cells = new List<TableCell>()
+					{
+						{ new TableCell() { Type = TableCellType.Row } }
+					};
+					
+					tableLayout.ExpandTable(cells, null, 0, null);
+				}
+			);
+		}
+		
+		[Test]
+		public void ExpandIncompleteTable_Should_ThrowNullReferenceException_When_CreatingNewColumn()
+		{
+			Assert.Throws<NullReferenceException>(() =>
+				{
+					var tableLayout = CreateInvalidLayoutUsingIncompleteFakeTable();
+					var cells = new List<TableCell>()
+					{
+						{ new TableCell() { Type = TableCellType.Column } }
+					};
+					
+					tableLayout.ExpandTable(cells, null, 0, null);
+				}
+			);
+		}
+		
+		[Test]
+		public void ExpandValidTable_Should_CreateChildObject_When_CreatingNewEmptyCell()
+		{
+			var parent = new GameObject();
+			var tableLayout = CreateValidLayoutUsingFakeTable();
 			var cells = new List<TableCell>()
 			{
-				{ new TableCell() { Type = TableCellType.StaticText } }
+				{ new TableCell() { Type = TableCellType.Empty, Refs = new [] {""} } }
 			};
 			
-			tableLayout.ExpandTable(cells, null, 0, null);
+			tableLayout.ExpandTable(cells, parent.transform, 0, null);
 			
-			Assert.Pass();
+			Assert.AreEqual(1, parent.transform.childCount);
 		}
 		
 		[Test]
-		public void ExpandTable_Should_NotHaveAnyEffect_When_DynamicTextCellHasNoReferences()
+		public void ExpandValidTable_Should_CreateChildObject_When_CreatingNewStaticText()
 		{
-			var tableLayout = CreateInvalidLayoutUsingNullTable();
+			var parent = new GameObject();
+			var tableLayout = CreateValidLayoutUsingFakeTable();
 			var cells = new List<TableCell>()
 			{
-				{ new TableCell() { Type = TableCellType.DynamicText } }
+				{ new TableCell() { Type = TableCellType.StaticText, Refs = new [] {""} } }
 			};
 			
-			tableLayout.ExpandTable(cells, null, 0, null);
+			tableLayout.ExpandTable(cells, parent.transform, 0, null);
 			
-			Assert.Pass();
+			Assert.AreEqual(1, parent.transform.childCount);
 		}
 		
 		[Test]
-		public void ExpandTable_Should_ThrowNullReferenceException_When_EmptyRowIsProcessedByNullTable()
+		public void ExpandValidTable_Should_CreateChildObject_When_CreatingNewDynamicText()
 		{
-			var tableLayout = CreateInvalidLayoutUsingNullTable();
+			var parent = new GameObject();
+			var tableLayout = CreateValidLayoutUsingFakeTable();
+			var cells = new List<TableCell>()
+			{
+				{ new TableCell() { Type = TableCellType.DynamicText, Refs = new [] {""} } }
+			};
+			
+			tableLayout.ExpandTable(cells, parent.transform, 0, null);
+			
+			Assert.AreEqual(1, parent.transform.childCount);
+		}
+		
+		[Test]
+		public void ExpandValidTable_Should_CreateChildObject_When_CreatingNewRow()
+		{
+			var parent = new GameObject();
+			var tableLayout = CreateValidLayoutUsingFakeTable();
 			var cells = new List<TableCell>()
 			{
 				{ new TableCell() { Type = TableCellType.Row } }
 			};
 			
-			tableLayout.ExpandTable(cells, null, 0, null);
+			tableLayout.ExpandTable(cells, parent.transform, 0, (a, b, c) => {});
 			
-			Assert.Pass();
+			Assert.AreEqual(1, parent.transform.childCount);
 		}
 		
 		[Test]
-		public void ExpandTable_Should_ThrowNullReferenceException_When_EmptyColumnIsProcessedByNullTable()
+		public void ExpandValidTable_Should_CreateChild_When_CreatingNewColumn()
 		{
-			var tableLayout = CreateInvalidLayoutUsingNullTable();
+			var parent = new GameObject();
+			var tableLayout = CreateValidLayoutUsingFakeTable();
 			var cells = new List<TableCell>()
 			{
 				{ new TableCell() { Type = TableCellType.Column } }
 			};
+
+			tableLayout.ExpandTable(cells, parent.transform, 0, (a, b, c) => {});
 			
-			tableLayout.ExpandTable(cells, null, 0, null);
+			Assert.AreEqual(1, parent.transform.childCount);
+		}
+		
+		[Test]
+		public void ExpandTable_Should_ThrowNullReferenceException_When_CellsWereNull()
+		{
+			Assert.Throws<NullReferenceException>(() =>
+				{
+					var tableLayout = CreateValidLayoutUsingFakeTable();
+
+					tableLayout.ExpandTable(null, null, 0, null);
+				}
+			);
+		}
+
+		[Test]
+		public void ExpandTable_Should_NotIncreaseChildCount_When_TryingToCreateNewEmptyCellWithoutReferenceId()
+		{
+			var parent = new GameObject();
+			var tableLayout = CreateValidLayoutUsingFakeTable();
+			var cells = new List<TableCell>()
+			{
+				{new TableCell() {Type = TableCellType.Empty}}
+			};
+
+			tableLayout.ExpandTable(cells, parent.transform, 0, null);
 			
-			Assert.Pass();
-		}
-		
-		/*[Test]
-		public void ExpandTable_Should_ThrowNullReferenceException_When_TableWasNotInitialized()
-		{
-			var cell = new TableCell();
-			cell.Refs.Add("0");
-			Cells = new List<TableCell>(){};
-			TargetScript.ExpandTable(Cells, null, 0, null);
-			Assert.Fail();
+			Assert.AreEqual(0, parent.transform.childCount);
+
 		}
 		
 		[Test]
-		public void ExpandTable_Should_AttachAllGameObjectsToRoot_When_ParentTransformWasNull()
+		public void ExpandTable_Should_NotIncreaseChildCount_When_TryingToCreateNewStaticTextWithoutReferenceId()
 		{
-			TargetScript.ExpandTable(null, null, 0, null);
-			Assert.Fail();
+			var parent = new GameObject();
+			var tableLayout = CreateValidLayoutUsingFakeTable();
+			var cells = new List<TableCell>()
+			{
+				{new TableCell() {Type = TableCellType.StaticText}}
+			};
+
+			tableLayout.ExpandTable(cells, parent.transform, 0, null);
+			
+			Assert.AreEqual(0, parent.transform.childCount);
 		}
 		
 		[Test]
-		public void ExpandTable_Should_NotThrowException_When_CallbackWasSetToNull()
+		public void ExpandTable_Should_NotIncreaseChildCount_When_TryingToCreateNewDynamicTextWithoutReferenceId()
 		{
-			TargetScript.ExpandTable(null, null, 0, null);
-			Assert.Fail();
+			var parent = new GameObject();
+			var tableLayout = CreateValidLayoutUsingFakeTable();
+			var cells = new List<TableCell>()
+			{
+				{new TableCell() {Type = TableCellType.DynamicText}}
+			};
+
+			tableLayout.ExpandTable(cells, parent.transform, 0, null);
+			
+			Assert.AreEqual(0, parent.transform.childCount);
 		}
 		
 		[Test]
-		public void ExpandTable_Should_ReturnIncreasedDepthByOne_When_CallbackIsCalled()
+		public void ExpandTable_Should_IncreaseChildCount_When_TryingToCreateNewRowWithoutReferenceId()
 		{
-			TargetScript.ExpandTable(null, null, 0, null);
-			Assert.Fail();
-		}*/
+			var parent = new GameObject();
+			var tableLayout = CreateValidLayoutUsingFakeTable();
+			var cells = new List<TableCell>()
+			{
+				{new TableCell() {Type = TableCellType.Row}}
+			};
+
+			tableLayout.ExpandTable(cells, parent.transform, 0, (a, b, c) => {});
+			
+			Assert.AreEqual(1, parent.transform.childCount);
+		}
+
+		[Test]
+		public void ExpandTable_Should_IncreaseChildCount_When_TryingToCreateNewColumnWithoutReferenceId()
+		{
+			var parent = new GameObject();
+			var tableLayout = CreateValidLayoutUsingFakeTable();
+			var cells = new List<TableCell>()
+			{
+				{new TableCell() {Type = TableCellType.Column}}
+			};
+
+			tableLayout.ExpandTable(cells, parent.transform, 0, (a, b, c) => {});
+			
+			Assert.AreEqual(1, parent.transform.childCount);
+		}
+		
+		[Test]
+		public void ExpandTable_Should_IncreaseDepthByOne_When_TryingToCreateNewRow()
+		{
+			var parent = new GameObject();
+			var tableLayout = CreateValidLayoutUsingFakeTable();
+			var cells = new List<TableCell>()
+			{
+				{new TableCell() {Type = TableCellType.Row}}
+			};
+
+			tableLayout.ExpandTable(cells, parent.transform, 0, (a, b, depth) =>
+			{
+				Assert.AreEqual(1, depth);
+			});
+		}
+
+		[Test]
+		public void ExpandTable_Should_IncreaseDepthByOne_When_TryingToCreateNewColumn()
+		{
+			var parent = new GameObject();
+			var tableLayout = CreateValidLayoutUsingFakeTable();
+			var cells = new List<TableCell>()
+			{
+				{new TableCell() {Type = TableCellType.Column}}
+			};
+
+			tableLayout.ExpandTable(cells, parent.transform, 0, (a, b, depth) =>
+			{
+				Assert.AreEqual(1, depth);
+			});
+		}
 	}
 }
