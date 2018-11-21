7cbefabe4634cfe5903f41f943a51a3b3a746b12
Lars Wobus <lars.wobus@web.de>
2018-11-21 21:30:59 +0100

    TableEditor initialization fully tested

--- IrregularTables/Assets/EssentialPackages/UI/IrregularTables/Scripts/Tests/TableEditorTest.cs
+++ IrregularTables/Assets/EssentialPackages/UI/IrregularTables/Scripts/Tests/TableEditorTest.cs
@@ -31,6 +31,14 @@ namespace EssentialPackages.UI.IrregularTables.Tests
 			fieldInfo.SetValue(tableData, new List<TableCell>());
 			return tableData;
 		}
+
+		private GameObject CreateInactiveGameObject()
+		{
+			var typeName = GetType().ToString();
+			var go = new GameObject(typeName);
+			go.SetActive(false);
+			return go;
+		}
 		
 		[OneTimeSetUp]
 		public void OneTimeSetUp()
@@ -69,42 +77,81 @@ namespace EssentialPackages.UI.IrregularTables.Tests
 			ScriptUnderTest = null;
 			Object.Destroy(GameObjectUnderTest);
 		}
-
-		/*[UnityTest]
-		public IEnumerator Awake_ThrowArgumentNullException_When_RootTransformWasNull()
+		
+		[UnityTest]
+		public IEnumerator Awake_ThrowArgumentNullException_When_TableBodyWasNull()
 		{
-			Assert.Throws<ArgumentNullException>(() =>
-				{
-					var typeName = GetType().ToString();
-					GameObjectUnderTest = new GameObject(typeName);
-					ScriptUnderTest = GameObjectUnderTest.AddComponent<TableEditor>();
-				}
-			);
+			var gameObjectUnderTest = CreateInactiveGameObject();
+			var scriptUnderTest = gameObjectUnderTest.AddComponent<TableEditor>();
+			
+			LogAssert.Expect(LogType.Exception, new Regex(@"ArgumentNullException:.*[\s\S].*_tableBody"));
+			
+			gameObjectUnderTest.SetActive(true);
+			
 			yield return null;
-		}*/
+		}
 		
 		[UnityTest]
 		public IEnumerator Awake_ThrowArgumentNullException_When_StyleWasNull()
 		{
-			var typeName = GetType().ToString();
-			GameObjectUnderTest = new GameObject(typeName);
-			GameObjectUnderTest.SetActive(false);
-			ScriptUnderTest = GameObjectUnderTest.AddComponent<TableEditor>();
-
-			var tableData = CreateDummyTableData();
-			
-			const BindingFlags bindingFlags = BindingFlags.NonPublic | BindingFlags.Instance;
-			var fieldInfo = Type.GetField("_tableData", bindingFlags);
-			fieldInfo.SetValue(ScriptUnderTest, tableData);
-			
-			fieldInfo = Type.GetField("_tableBody", bindingFlags);
-			fieldInfo.SetValue(ScriptUnderTest, new GameObject().transform);
+			var gameObjectUnderTest = CreateInactiveGameObject();
+			var scriptUnderTest = gameObjectUnderTest.AddComponent<TableEditor>();
+	
+			var fieldInfo = Type.GetField("_tableBody", BindingFlags);
+			fieldInfo.SetValue(scriptUnderTest, new GameObject().transform);
 			
 			LogAssert.Expect(LogType.Exception, new Regex(@"ArgumentNullException:.*[\s\S].*_style"));
 			
-			GameObjectUnderTest.SetActive(true);
+			gameObjectUnderTest.SetActive(true);
 			
 			yield return null;
+			
+			//var tableData = CreateDummyTableData();
+			//var fieldInfo = Type.GetField("_tableData", bindingFlags);
+			//fieldInfo.SetValue(ScriptUnderTest, tableData);
+		}
+
+		[UnityTest]
+		public IEnumerator Awake_Should_ThrowNullReferenceException_When_TableDataWasNull()
+		{
+			var gameObjectUnderTest = CreateInactiveGameObject();
+			var scriptUnderTest = gameObjectUnderTest.AddComponent<TableEditor>();
+	
+			var fieldInfo = Type.GetField("_tableBody", BindingFlags);
+			fieldInfo.SetValue(scriptUnderTest, new GameObject().transform);
+			
+			fieldInfo = Type.GetField("_style", BindingFlags);
+			fieldInfo.SetValue(scriptUnderTest, ScriptableObject.CreateInstance<TableStyle>());
+			
+			LogAssert.Expect(LogType.Exception, new Regex(@"NullReferenceException:"));
+			
+			gameObjectUnderTest.SetActive(true);
+			
+			yield return null; 
+		}
+		
+		[UnityTest]
+		public IEnumerator Awake_Should_Succeed_When_AllFieldsWereInitialized()
+		{
+			var gameObjectUnderTest = CreateInactiveGameObject();
+			var scriptUnderTest = gameObjectUnderTest.AddComponent<TableEditor>();
+	
+			var fieldInfo = Type.GetField("_tableBody", BindingFlags);
+			fieldInfo.SetValue(scriptUnderTest, new GameObject().transform);
+			
+			fieldInfo = Type.GetField("_style", BindingFlags);
+			fieldInfo.SetValue(scriptUnderTest, ScriptableObject.CreateInstance<TableStyle>());
+
+			var tableData = new TableData();
+			fieldInfo = Type.GetField("_tableData", BindingFlags);
+			fieldInfo.SetValue(scriptUnderTest, tableData);
+			fieldInfo = typeof(TableData).GetField("_body", BindingFlags);
+			fieldInfo.SetValue(tableData, new List<TableCell>());
+			
+			gameObjectUnderTest.SetActive(true);
+			
+			Assert.Pass();
+			yield return null; 
 		}
 
 		/*[UnityTest]
