bc8a772402e5ebd0e0bc84216612895adc248615
Lars Wobus <lars.wobus@web.de>
2018-11-22 10:58:13 +0100

    Start outsourcing serialized fields

---
+++ IrregularTables/Assets/EssentialPackages/UI/IrregularTables/Scripts/Data/TableConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace EssentialPackages.UI.IrregularTables.Data
+{
+	public class TableConfiguration : MonoBehaviour
+	{
+		[SerializeField] private TableData _tableData;
+		[SerializeField] private Transform _tableBody;
+		[SerializeField] private TableStyle _style; 
+
+		public TableData TableData => _tableData;
+		public Transform Transform => _tableBody;
+		public TableStyle TableStyle => _style;
+	}
+}
