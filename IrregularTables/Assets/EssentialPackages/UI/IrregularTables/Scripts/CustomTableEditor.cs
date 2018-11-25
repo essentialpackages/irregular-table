4f8598828b06e9b6f2f398f258adf2064820abac
Lars Wobus <lars.wobus@web.de>
2018-11-25 22:05:23 +0100

    More general method to create custom rows

--- IrregularTables/Assets/EssentialPackages/UI/IrregularTables/Scripts/CustomTableEditor.cs
+++ IrregularTables/Assets/EssentialPackages/UI/IrregularTables/Scripts/CustomTableEditor.cs
@@ -1,33 +1,65 @@
 ﻿using System;
+using System.Collections;
 using EssentialPackages.UI.IrregularTables.Data;
+using UnityEngine;
+using Random = System.Random;
 
 namespace EssentialPackages.UI.IrregularTables
 {
-	public class CustomTableEditor : TableEditor
-	{
-		protected new void Awake()
-		{
-			base.Awake();
-			// Test
-			CreateCustomRow("player1", "gamepad", "nothing", "5", "Has no items");
-		}
-		
-		public void CreateCustomRow(string playerNo, string controls, string defaultText, string lives, string description)
-		{
-			var firstUuid = Guid.NewGuid().ToString();
-			var secondUuid = Guid.NewGuid().ToString();
-			var thirdUuid = Guid.NewGuid().ToString();
-			AddItemData(firstUuid, TableCellType.StaticText, new []{playerNo, controls});
-			AddItemData(secondUuid, TableCellType.DynamicText, new []{defaultText});
-			AddItemData(thirdUuid, TableCellType.StaticText, new []{lives, description});
+    /// <summary>
+    /// Example class about how tables should be extended.
+    /// </summary>
+    public class CustomTableEditor : TableEditor
+    {
+        protected new void Awake()
+        {
+            base.Awake();
+        }
 
-			var id = Guid.NewGuid().ToString();
-			AddItemData(id, TableCellType.Row, new []{firstUuid, secondUuid, thirdUuid});
-			
-			GetRootData()?.Refs.Add(id);
+        private IEnumerator Start()
+        {
+            var random = new Random();
+            
+            while(true)
+            {
+                CreateCustomRow(
+                    random.Next().ToString(),
+                    random.Next().ToString(),
+                    random.Next().ToString(),
+                    random.Next().ToString(),
+                    random.Next().ToString());
+                yield return new WaitForSeconds(1f);
+            }
+        }
+        
+        /// <summary>
+        /// Example method which creates three groups with overall five columns.
+        /// </summary>
+        /// <example>
+        /// CreateCustomRow("Lorem", "ipsum", "dolor", "sit", "amet");
+        /// </example>
+        /// <param name="column1">Column 1</param>
+        /// <param name="column2">Column 2</param>
+        /// <param name="column3">Column 3</param>
+        /// <param name="column4">Column 4</param>
+        /// <param name="column5">Column 5</param>
+        public void CreateCustomRow(string column1, string column2, string column3, string column4,
+            string column5)
+        {
+            var groupId1 = Guid.NewGuid().ToString();
+            var groupId2 = Guid.NewGuid().ToString();
+            var groupId3 = Guid.NewGuid().ToString();
+            AddItemData(groupId1, TableCellType.StaticText, new[] {column1, column2});
+            AddItemData(groupId2, TableCellType.DynamicText, new[] {column3});
+            AddItemData(groupId3, TableCellType.StaticText, new[] {column4, column5});
 
-			var parent = GetRootItem();
-			FillTable(new[]{id}, parent, 1);
-		}
-	}
+            var rowId = Guid.NewGuid().ToString();
+            AddItemData(rowId, TableCellType.Row, new[] {groupId1, groupId2, groupId3});
+
+            GetRootData()?.Refs.Add(rowId);
+
+            var parent = GetRootItem();
+            FillTable(new[] {rowId}, parent, 1);
+        }
+    }
 }
